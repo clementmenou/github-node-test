e525cbc52c6187d7fbaac5b915377eadb9e824be
Clément Menou <clementmenou0@gmail.com>
2023-10-04 22:07:59 +0200

    Node v1 : Global

---
+++ global.js
@@ -0,0 +1,12 @@
+// Global object
+
+// setTimeout(() => {
+//     console.log('time out')
+// }, 3000)
+
+// Usefull vars
+console.log(__dirname);
+console.log(__filename);
+
+// Doesn't work because linked to DOM not avaliable in back
+// console.log(document.querySelector) 
---
+++ test.js
@@ -0,0 +1,3 @@
+const name1 = "mario";
+
+console.log(name1);
